eb86df94c2f8ef8eef3bdc64458aff71875cfe1a
shelvin <weixianqing@gmail.com>
2015-11-06 01:15:23 +0800

    example of publishing underlying mutable state variables  2;

--- src/com/shelvin/java/concurrent/SafePoint.java
+++ src/com/shelvin/java/concurrent/SafePoint.java
@@ -6,11 +6,15 @@ package com.shelvin.java.concurrent;
 public class SafePoint
 {
     private int x,y;
+
     private SafePoint(int[] a)
     {
         this(a[0],a[1]);
     }
 
+    //这个构造函数能在多线程下正常工作吗?想办法测试;如果能保证并发,
+    //时怎么实现的?(多个线程同时调用这个构造函数,我感觉时会造成并发问题的)
+    //这里私有构造器的目的时什么,和这个公有的构造器有没有关系?
     public SafePoint(int x, int y)
     {
         this.x = x;
