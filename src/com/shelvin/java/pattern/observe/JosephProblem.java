acfa159887d03e48f054ee109df763ae092aa422
shelvin <weixianqing@foxmail.com>
2015-11-11 15:04:39 +0800

    //shelvin test about class initial,more veri needed to be done.

---
+++ src/com/shelvin/java/pattern/observe/JosephProblem.java
@@ -0,0 +1,19 @@
+package com.shelvin.java.pattern.observe;
+
+public class JosephProblem
+{
+    public static void main(String[] args)
+    {
+        int total = 50;
+        int interval = 3;
+        int i, people = 0;
+        //No.1
+        //开始写代码，求出最后留下来的人
+        for (i = 2; i < total; i++)
+        {
+            people = (people+interval)%i;
+        }
+        //end_code
+        System.out.println("最后留下来的人是:" + (people + 1));
+    }
+}
