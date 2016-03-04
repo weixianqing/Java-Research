2148fbfc47f69343414dc3e2b7f68f95a989fa40
shelvin zhou <shelvin@IP-58-154-240-134.neu.edu.cn>
2016-03-04 23:40:54 +0800

    Leetcode -- ReverseInteger

--- src/leetcode/ReverseInteger.java
+++ src/leetcode/ReverseInteger.java
@@ -5,9 +5,49 @@ package leetcode;
  */
 public class ReverseInteger
 {
-    public int reverse(int x)
+    /*
+    without consider integer overflow
+     */
+    public int reverse(int input)
+    {
+        int sum = 0;
+        while (input != 0)
+        {
+            int temp = input % 10;
+            sum = sum*10+temp;
+            input /= 10;
+        }
+
+        return sum;
+    }
+
+    public int reverseWithuotOverflow()
     {
         int result = 0;
+
+
+
         return result;
+    }
+
+    /*
+    Test mod function
+    Modding of negative's process.
+     */
+    public static int negMod(int input)
+    {
+        int result = 0;
+        result = input % (-3);
+        return result;
+    }
+
+    public static void main(String[] args)
+    {
+        ReverseInteger ri = new ReverseInteger();
+        int sum = ri.reverse(-1233);
+        System.out.println(sum);
+
+        int result = negMod(8);
+        System.out.println(result);
     }
 }
