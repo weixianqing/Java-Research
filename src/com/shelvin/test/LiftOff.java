e5812ad51a101cb508109247d8fd8f444e89095d
shelvin zhou <shelvin@IP-58-154-240-134.neu.edu.cn>
2016-03-21 22:49:16 +0800

    code reformt

--- src/com/shelvin/test/LiftOff.java
+++ src/com/shelvin/test/LiftOff.java
@@ -5,23 +5,25 @@ package com.shelvin.test;
  */
 public class LiftOff implements Runnable
 {
-    protected int countDown=10;
-    private static int taskCount=0;
-    private final int id=taskCount++;
+    protected int countDown = 10;
+    private static int taskCount = 0;
+    private final int id = taskCount++;
 
-    public LiftOff() {
+    public LiftOff()
+    {
 
     }
 
     public String status()
     {
-        return "#"+id+"("+
-                (countDown>0?countDown:"liftoff!")+")";
+        return "#" + id + "(" +
+                (countDown > 0 ? countDown : "liftoff!") + ")";
     }
 
     @Override
-    public void run() {
-        while (countDown-->0)
+    public void run()
+    {
+        while (countDown-- > 0)
         {
             System.out.println(status());
             Thread.yield();
