13b437fc5d8f94d2b12533a473dab72ee61f5fc2
Shelvin Zhou <shelvin@zhou.local>
2016-07-09 00:35:04 +0530

    Integer Palindrome

---
+++ src/leetcode/PalindromeInteger.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+/**
+ * Created by shelvin on 9/7/16.
+ */
+public class PalindromeInteger
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    public boolean isPalindrome(int ipt)
+    {
+        boolean res = false;
+
+
+        return res;
+    }
+}
