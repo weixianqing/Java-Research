fd931ee95b14735d290f2f653896a1163dbefdb8
Shengwei.Zhou <weixianqing@gmail.com>
2016-09-29 20:49:56 +0530

    reformating code

--- src/leetcode/MaZouRi.java
+++ src/leetcode/MaZouRi.java
@@ -4,12 +4,14 @@ package leetcode;
  * Created by Shelvin Zhou on 2016/9/29 at 19:37.
  * 马走日
  */
-public class MaZouRi {
+public class MaZouRi
+{
 
     int[][] move = {{-1, -2}, {-2, -1}, {-2, 1}, {-1, 2}, {1, 2}, {2, 1}, {2, -1}, {1, -2}};
     int ansNum = 1;
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         int[][] board = new int[5][5];
         int count = 1;
         board[0][0] = 1;
@@ -18,28 +20,35 @@ public class MaZouRi {
         return;
     }
 
-    public void DFS(int[][] board, int x, int y, int count) {
+    public void DFS(int[][] board, int x, int y, int count)
+    {
 
         int nextX;
         int nextY;
 
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < 8; i++)
+        {
             nextX = x + move[i][0];
             nextY = y + move[i][1];
 
-            if (isValide(board, nextX, nextY)) {
-                if (count != 5 * 5 - 1) {
+            if (isValide(board, nextX, nextY))
+            {
+                if (count != 5 * 5 - 1)
+                {
                     count++;
                     board[nextX][nextY] = count;
                     DFS(board, nextX, nextY, count);
                     board[nextX][nextY] = 0;
                     count--;
-                } else {
+                } else
+                {
                     board[nextX][nextY] = 25;
                     System.out.println("----------------------------------------");
                     System.out.println("solution:" + ansNum++);
-                    for (int j = 0; j < 5; j++) {
-                        for (int k = 0; k < 5; k++) {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        for (int k = 0; k < 5; k++)
+                        {
                             System.out.print(board[j][k] + "  ");
                         }
                         System.out.println();
@@ -51,8 +60,10 @@ public class MaZouRi {
         }
     }
 
-    public boolean isValide(int[][] board, int x, int y) {
-        if (x >= 0 && x <= 4 && y >= 0 && y <= 4 && board[x][y] == 0) {
+    public boolean isValide(int[][] board, int x, int y)
+    {
+        if (x >= 0 && x <= 4 && y >= 0 && y <= 4 && board[x][y] == 0)
+        {
             return true;
         }
         return false;
