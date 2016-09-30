0491629abe9a95c34c69839bbde6eb1039af284e
Shengwei.Zhou <weixianqing@gmail.com>
2016-09-30 20:12:16 +0530

    reformating code

--- src/leetcode/Triangle_120.java
+++ src/leetcode/Triangle_120.java
@@ -4,14 +4,18 @@ package leetcode;
 /**
  * Created by Shelvin Zhou on 2016/9/30 at 18:46.
  */
-public class Triangle_120 {
+public class Triangle_120
+{
 
     //top-down
-    public int minPathSum1(int[][] triangle) {
-        if (triangle == null && triangle.length == 0) {
+    public int minPathSum1(int[][] triangle)
+    {
+        if (triangle == null && triangle.length == 0)
+        {
             return -1;
         }
-        if (triangle[0] == null && triangle[0].length == 0) {
+        if (triangle[0] == null && triangle[0].length == 0)
+        {
             return -1;
         }
 
@@ -19,19 +23,23 @@ public class Triangle_120 {
         int[][] fun = new int[len][len];
         fun[0][0] = triangle[0][0];
 
-        for (int i = 1; i < len; i++) {
+        for (int i = 1; i < len; i++)
+        {
             fun[i][0] = fun[i - 1][0] + triangle[i][0];
             fun[i][i] = fun[i - 1][i - 1] + triangle[i][i];
         }
 
-        for (int i = 1; i < len; i++) {
-            for (int j = 1; j < i; j++) {
+        for (int i = 1; i < len; i++)
+        {
+            for (int j = 1; j < i; j++)
+            {
                 fun[i][j] = Math.min(fun[i - 1][j], fun[i - 1][j - 1]) + triangle[i][j];
             }
         }
 
         int best = fun[len - 1][0];
-        for (int i = 1; i < len; i++) {
+        for (int i = 1; i < len; i++)
+        {
             best = Math.min(best, fun[len - 1][i]);
         }
 
@@ -40,12 +48,15 @@ public class Triangle_120 {
 
 
     //down-top
-    public int minPathSum2(int[][] triangle) {
+    public int minPathSum2(int[][] triangle)
+    {
 
-        if (triangle == null && triangle.length == 0) {
+        if (triangle == null && triangle.length == 0)
+        {
             return -1;
         }
-        if (triangle[0] == null && triangle[0].length == 0) {
+        if (triangle[0] == null && triangle[0].length == 0)
+        {
             return -1;
         }
 
@@ -53,12 +64,15 @@ public class Triangle_120 {
         int[][] fun = new int[len][len];
         fun[0][0] = triangle[0][0];
 
-        for (int i = 0; i < len; i++) {
+        for (int i = 0; i < len; i++)
+        {
             fun[len - 1][i] = triangle[len - 1][i];
         }
 
-        for (int i = len - 2; i >= 0; i--) {
-            for (int j = 0; j < i; j++) {
+        for (int i = len - 2; i >= 0; i--)
+        {
+            for (int j = 0; j < i; j++)
+            {
                 fun[i][j] = Math.min(fun[i + 1][j], fun[i + 1][j + 1]) + triangle[i][j];
             }
         }
@@ -67,12 +81,15 @@ public class Triangle_120 {
     }
 
     //memorization
-    public int dfsSearch(int[][] triangel, int[][] minSum, int x, int y) {
-        if (x >= minSum.length) {
+    public int dfsSearch(int[][] triangel, int[][] minSum, int x, int y)
+    {
+        if (x >= minSum.length)
+        {
             return 0;
         }
 
-        if (minSum[x][y] != Integer.MAX_VALUE) {
+        if (minSum[x][y] != Integer.MAX_VALUE)
+        {
             return minSum[x][y];
         }
 
@@ -81,19 +98,24 @@ public class Triangle_120 {
         return minSum[x][y];
     }
 
-    public int minPathSum3(int[][] triangle) {
+    public int minPathSum3(int[][] triangle)
+    {
 
-        if (triangle == null && triangle.length == 0) {
+        if (triangle == null && triangle.length == 0)
+        {
             return -1;
         }
-        if (triangle[0] == null && triangle[0].length == 0) {
+        if (triangle[0] == null && triangle[0].length == 0)
+        {
             return -1;
         }
 
         int len = triangle.length;
         int[][] minSum = new int[triangle.length][triangle.length];
-        for (int i = 0; i < len; i++) {
-            for (int j = 0; j < i; j++) {
+        for (int i = 0; i < len; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
                 minSum[i][j] = Integer.MAX_VALUE;
             }
         }
