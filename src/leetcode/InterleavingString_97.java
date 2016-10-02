277e9acad6835ab7a6caebf44ed7d47fab76bbd9
Shengwei.Zhou <weixianqing@gmail.com>
2016-10-02 23:56:19 +0530

    is interleaved string by dynamic programming optimized with rolling array(sliding array or sliding window)

--- src/leetcode/InterleavingString_97.java
+++ src/leetcode/InterleavingString_97.java
@@ -49,4 +49,43 @@ public class InterleavingString_97
 
         return f[len1][len2];
     }
+
+    public boolean isInterleavedOptimizedSpace(String s1, String s2, String s3)
+    {
+        int len1 = s1.length();
+        int len2 = s2.length();
+        int len3 = s3.length();
+
+        if (len1 + len2 != len3)
+        {
+            return false;
+        }
+
+        if (len1 < len2)
+        {
+            return isInterleavedOptimizedSpace(s2, s1, s3);
+        }
+
+        boolean[] f = new boolean[len2 + 1];
+        for (int i = 1; i <= len2; i++)
+        {
+            f[i] = true;
+        }
+
+        for (int i = 1; i <= len2; i++)
+        {
+            f[i] = s2.charAt(i - 1) == s3.charAt(i - 1) && f[i - 1];
+        }
+
+        for (int i = 1; i <= len1; i++)
+        {
+            f[0] = s1.charAt(i - 1) == s3.charAt(i - 1) && f[0];
+            for (int j = 1; j <= len2; j++)
+            {
+                f[j] = ((s1.charAt(i - 1) == s3.charAt(i + j - 1) && f[j]) || (s2.charAt(j - 1) == s3.charAt(i + j - 1) && f[j - 1]));
+            }
+        }
+
+        return f[len2];
+    }
 }
