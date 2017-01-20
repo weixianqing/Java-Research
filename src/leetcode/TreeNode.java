f8761d2a47660acc3bb9e6b74510767404a5e141
Shengwei.Zhou <weixianqing@gmail.com>
2017-01-20 18:10:53 +0800

    inorder traversal

---
+++ src/leetcode/TreeNode.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * Created by shelvin on 17/1/17 at 13:02.
+ */
+
+public class TreeNode
+{
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val)
+    {
+        this.val = val;
+    }
+}
