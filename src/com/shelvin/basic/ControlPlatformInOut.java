b8a103db3ab16e1898ac1450abd29acdd381f7ec
zhoushengwei <zhoushengwei@hisense.com>
2019-02-20 17:24:26 +0800

    bug04

--- src/com/shelvin/basic/ControlPlatformInOut.java
+++ src/com/shelvin/basic/ControlPlatformInOut.java
@@ -15,6 +15,7 @@ public class ControlPlatformInOut
         System.out.println("请输入一维数组，每个元素以单个空格间隔，最后一个元素不要添加空格，直接以回车键结束输入");
         System.out.println("bug01 branch test");
         System.out.println("888888888");
+        System.out.println("bug04 branch test");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = null;
         try
